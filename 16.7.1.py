d2629e363e9a1de174c3f4517d84deae6b3f2842
Dmitry <104850369+drahmatullin5@users.noreply.github.com>
2022-08-10 13:26:25 +0300

    Create 16.7.1.py

---
+++ 16.7.1.py
@@ -0,0 +1,8 @@
+class cat:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def cat_info(self):
+        print(f"Имя: {self.name} Возраст: {self.age} Пол: {self.gender}")
